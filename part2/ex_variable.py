05c81aad93a069ee346c82b86a5f43a997472147
안우진 <wjahn@outlook.com>
2017-06-07 04:34:51 +0900

    part2 소스파일

---
+++ part2/ex_variable.py
@@ -0,0 +1,5 @@
+name = '김아무개'
+
+season = '겨울'
+print('지금은', season, '입니다.')
+
---
+++ part2/variable.py
@@ -0,0 +1,7 @@
+print('안녕! 나는 지구인이야. 나는 다리가 4개 있어.')
+
+print('안녕! 나는', '지구인', '이야. 나는 다리가', 4, '개 있어.')
+
+identity = '지구인'
+number_of_legs = 4
+print('안녕! 나는', identity, '이야. 나는 다리가', number_of_legs, '개 있어.')

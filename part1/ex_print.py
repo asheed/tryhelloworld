8c4980da869d68b49b0f43701939cf63e6aba2d8
안우진 <wjahn@outlook.com>
2017-06-07 03:47:12 +0900

    part1 소스파일

---
+++ part1/ex_print.py
@@ -0,0 +1,9 @@
+print('Hello World')
+print('Hello World')
+print('Hello World')
+
+# for _ in range(3):
+#     print('Hello World')
+
+print(3*'Hello World\n',end='')
+
---
+++ part1/hello.py
@@ -0,0 +1,5 @@
+print('Hello, world!')
+
+# print('Hello world)
+# 이 코드는 에러를 발생합니다.
+# 주석을 해제하고 실행해 보세요.

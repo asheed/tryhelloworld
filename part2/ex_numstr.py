efaa4eab0967d3b23c362852bbca678d988b5fe4
안우진 <wjahn@outlook.com>
2017-06-07 21:44:18 +0900

    주석, 숫자와 문자열 부분 코드 추가

---
+++ part2/ex_numstr.py
@@ -0,0 +1,21 @@
+# step1
+a = 33
+b = 3
+
+sum = 33 + 3
+multiply = 33 * 3
+divide = 33 / 3
+remainder = 33 % 3
+power = 33 ** 3
+
+print("sum :", sum)
+print("multiply :", multiply)
+print("divide :", divide)
+print("remainder :", remainder)
+print("power :", power)
+
+# step2
+birth_year = '1985'
+birth_date = '0502'
+year_and_date = birth_year + birth_date
+print(year_and_date)
---
+++ part2/number.py
@@ -0,0 +1,23 @@
+my_name = 'Python'  # 문자열
+my_age = 25          # 숫자
+# my_age = '25'      # 문자열일 때는 11라인의 print문 실행시에 오류 발생
+print(my_name, '은 이제', my_age, '살')
+
+# 한해가 지나 한살을 더 먹었어요.
+# my_next_age = 26
+# my_next_age = my_age + 1
+my_age = my_age + 1
+# my_age += 1
+print(my_name, '은 내년에는', my_age, '살')
+
+text = '2015' + '1991'
+number = 2015 + 1991
+
+text_plus_number = '2015' + 1991
+text_mult_number = '2015' * 1991
+
+print(text)
+print(number)
+
+print(text_plus_number)
+print(text_mult_number)
